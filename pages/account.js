f757ad32f9c8511e851d21b987dc7147c6567eeb
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-02-14 14:58:29 -0300

    14/02 14:58

---
+++ pages/account.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LayoutBasico from "../layouts/LayoutBasico"
+
+export default function Account(){
+     return(
+         <LayoutBasico className="account">
+             <h1>Estamos na minha conta</h1>
+         </LayoutBasico>
+     )
+
+
+}
