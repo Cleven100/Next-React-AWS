31f3fa4cea5e7d4088aa401ec3f63e8780550db7
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-02-16 22:42:19 -0300

    16/02 22:42

--- components/Account/ChangePasswordForm/ChangePasswordForm.js
+++ components/Account/ChangePasswordForm/ChangePasswordForm.js
@@ -1,27 +1,43 @@
 import React from "react";
 import { Form, Button} from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { toast } from "react-toastify";
 
 
 export default function ChangePasswordForm(props){
 
     const {user, logout} = props;
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: Yup.object(validationSchema()),
+        onSubmit: (formData) => {
+            console.log(formData);
+        }
+    })
    
     return(
         <div className="change-password-form">
             <h4><hr/>Modificar sua senha</h4>
-            <Form>
+            <Form onSubmit={formik.handleSubmit}>
                 <Form.Group widths="equal">
                     <Form.Input 
                        name="password"
                        type="password"
                        placeholder="Sua nova senha"
+                       onChange={formik.handleChange}
+                       value={formik.values.passowrd}
+                       error={formik.errors.password}
                         
                     />
 
                       <Form.Input 
                        name="repeatPassword"
-                       type="repeatPassword"
+                       type="password"
                        placeholder="Repita sua nova senha"
+                       onChange={formik.handleChange}
+                       value={formik.values.repeatPassowrd}
+                       error={formik.errors.repeatPassword}
                         
                     />
                   
@@ -34,4 +50,20 @@ export default function ChangePasswordForm(props){
 
 
 
+}
+
+
+function initialValues(){
+    return {
+        password: "",
+        repeatPassword: "",
+    }
+}
+
+
+function validationSchema() {
+    return {
+        password: Yup.string().required(true).oneOf([Yup.ref("repeatPassword")], true),
+        repeatPassword: Yup.string().required(true).oneOf([Yup.ref("password")], true),
+    }
 }
