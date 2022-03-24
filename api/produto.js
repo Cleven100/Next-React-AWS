cff5c468b8e9e8a274ac181b552714be83d7cffa
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-03-24 20:36:28 -0300

    24/03

--- api/produto.js
+++ api/produto.js
@@ -1,15 +1,15 @@
 import { BASE_PATH } from "../utils/constants";
 
-
-export async function getLastProdutosApi(limit){
-    try {
-        const limitItems = `_limit=${limit}`
-       const url = `${BASE_PATH}/produtos?${limit}`;       
- 
-
-
-    } catch (error) {
-        console.log(error);
-        return null;
-    }
+export async function getLastProdutosApi(limit) {
+  try {
+    const limitItems = `_limit=${limit}`;
+    const sortItem = "_sort=createdAt:desc";
+    const url = `${BASE_PATH}/produtos?${limitItems}&${sortItem}`;
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,28 @@ import { MenuPlatforms } from "../components/Header/MenuSite/MenuSite"
 import { Search } from "../components/Header/TopBar/TopBar"
 import { getPlatFormsApi } from "../api/platform"
 import React, { useState, useEffect } from "react";
+import { getLastProdutosApi } from "../api/produto";
+import {size} from "lodash";
+
 
 export default function Home() {
 
   const [platforms, setPlatforms] = useState([]);
+  const [produtos, setProdutos] = useState(null);
+  console.log(produtos);
+
+   useEffect(() => {
+     (async ()  =>{
+       const response = await getLastProdutosApi(50);
+       if(size(response) > 0) {
+         setProdutos(response)
+       } else {
+         setProdutos([]);
+       }
+     })();
+   }, [])
+   
+
 
   useEffect(() => {
     (async () => {
