4eb195f06a1421f2b8e3913427bbc37def49056d
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-03-24 20:18:11 -0300

    24/03

--- api/produto.js
+++ api/produto.js
@@ -1,1 +1,15 @@
 import { BASE_PATH } from "../utils/constants";
+
+
+export async function getLastProdutosApi(limit){
+    try {
+        const limitItems = `_limit=${limit}`
+       const url = `${BASE_PATH}/produtos?${limit}`;       
+ 
+
+
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
