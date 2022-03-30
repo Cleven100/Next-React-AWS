efc0a6e1bf7430ebc0cdb103da5459671a1ed275
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-03-30 19:55:07 -0300

    30/03

--- components/ListProdutos/ListProdutos.js
+++ components/ListProdutos/ListProdutos.js
@@ -1,15 +1,38 @@
 import React from 'react'
+import { Image, Grid } from "semantic-ui-react";
+import Link from "next/link"
 import { map } from "lodash";
 
 export default function ListProdutos(props) {
     const {produtos} = props;
   return (
     <div className='list-produtos'>
-      {map(produtos, (produto) => (
-        <h3>{produto.title}</h3>
+      <Grid>
+          <Grid.Row columns={5}>
+             {map(produtos, (produto) => (
+        <Produto produto={produto}/>
       ))}
         
+          </Grid.Row>
+      </Grid>
+     
     </div>
   )
 }
 
+
+
+function Produto(props) {
+  const { produto } = props;
+  return (
+   <Grid.Column className="list-produto__produto">
+       <Link href={`/${produto.url}`}>
+         <a>
+            <div className="list-produtos__produto-poster">
+              <Image src={produto.poster.url} alt={produto.title} />
+            </div>
+         </a>
+       </Link>
+   </Grid.Column>
+  )
+}
