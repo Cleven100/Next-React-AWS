6877097e358589e0546df310159624038fc52ff1
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-06 18:15:24 -0300

    06/04

--- components/Pagination/Pagination.js
+++ components/Pagination/Pagination.js
@@ -1,14 +1,29 @@
 import React from "react";
 import {Pagination as PaginationSU } from "semantic-ui-react";
 import { useRouter } from "next/router";
+import queryString from "query-string"
 
 
 export default function Pagination(props) {
     const {totalProdutos, page, limitPerPage } = props;
+    const totalPages = Math.ceil(totalProdutos / limitPerPage);
+
+   const goToPage = (newPage) => {
+       console.log(newPage);
+   }
 
   return (
-      <div>
-          <h1>Pagination...</h1>
+      <div className="pagination">
+          <PaginationSU 
+          defaultActivePage={page}
+          totalPages={totalPages}
+          firstItem={null}
+          lastItem={null}
+          onPageChange={goToPage}
+          boundaryRange={0}
+          siblingRange={1}
+          ellipsisItem={null}
+          />
       </div>
   )
 
